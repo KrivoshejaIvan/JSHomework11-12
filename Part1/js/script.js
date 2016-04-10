3ba570a68ecc44d329bf1bac9f76f2d1b831520b
KrivoshejaIvan <ranger3331@gmail.com>
2016-04-10 23:21:12 +0300

    add JSHomework11-12

---
+++ Part1/js/script.js
@@ -0,0 +1,69 @@
+﻿$(function () {
+    var html= $('#page').html();
+    var title = [
+        {
+            title: 'Кривошея Іван Володимирович'
+        },
+        {
+            student:'Студент Харківського Державного Університету Харчових Технологій'
+        }
+        ];
+    var thumbnail = [
+        {
+            thumbnail: '1.jpg'
+        }
+    ];
+    var  question = [
+        {
+            question: 'Чому я хочу пізнати Frontend'
+        }
+    ];
+    var  answer = [
+        {
+            answer:'Хочу реалізуватися в житті'
+        },
+        {
+            answer:'Хочу мати роботу яку поллюбляєш'
+        },
+        {
+            answer:'Мене не влаштовує професія яку я отримаю в своєму вузі'
+        }
+    ];
+    var number = [
+        {
+            number:'Мій контактний телефон'
+        }
+    ];
+    var number2 = [
+        {
+            number2:'380505857947'
+        }
+    ];
+    var profile = [
+        {
+            profile:'Мій профіль в ВК'
+        }
+    ];
+    var link = [
+        {
+            link:'http://vk.com/feed'
+        }
+    ];
+    var feedback = [
+        {
+            feedback:'Мій федбек'
+        }
+    ];
+    var content = tmpl(html,{
+        data:title,
+        dataImage: thumbnail,
+        dataP: question,
+        dataLi: answer,
+        dataPhone: number,
+        dataNumber: number2,
+        dataProfile: profile,
+        dataLink: link,
+        dataFeedback: feedback
+    });
+    $('body').append(content)
+});
